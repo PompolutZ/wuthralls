0cdeacba3100b794cc257b2f0b377d205da6e994
Oleh Lutsenko <oleh.lutsenko@exopen.se>
2020-01-09 13:40:52 +0100

    fix service worker import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './firebase';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
