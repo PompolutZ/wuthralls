713c6c6a50b6feb266edf2f0ad2a82338be16889
Oleh Lutsenko <pompolutz@gmail.com>
2021-02-17 20:45:11 +0100

    update readme with some thoughts about this very circus

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,27 @@
+import seedrandom from "seedrandom";
+
+const rng = seedrandom(
+    "7v1IePLKA7oRefKddVznOklLczjSIDdaMeE0SUx7WPKAi8GUbAgDGfvgjswt",
+    { entropy: true }
+);
+
+export const shuffle = (a) => {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(rng() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+};
+
+export function getDieRollResult() {
+    const min = 1;
+    const max = 6;
+    return Math.floor(rng() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 export const sortByIdAsc = ({ id: prevId }, { id: nextId }) => {
     return prevId.localeCompare(nextId);
 };
