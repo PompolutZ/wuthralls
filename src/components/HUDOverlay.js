d21b54dfee0878c31dd0124acd4dfbbfda05a46f
Oleh Lutsenko <pompolutz@gmail.com>
2021-03-08 21:29:14 +0100

    redesign HUDOverlay to be portal based

--- src/components/HUDOverlay.js
+++ src/components/HUDOverlay.js
@@ -1,82 +1,101 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import AddIcon from "@material-ui/icons/Add";
 import SaveIcon from "@material-ui/icons/Save";
 import PropTypes from "prop-types";
 
+function Overlay(props) {
+    const [portalHost, setPortalHost] = useState(undefined);
+
+    useLayoutEffect(() => {
+        const domNode = document.createElement("div");
+        domNode.setAttribute("id", "overlay");
+        document.body.appendChild(domNode);
+        setPortalHost(domNode);
+
+        document.body.style.height = "100vh";
+        document.body.style.overflowY = "hidden";
+        document.body.style.paddingRight = "15px";
+
+        const rootElement = document.getElementById("root");
+        rootElement.style.filter = "blur(3px)";
+
+        return () => {
+            const node = document.getElementById("overlay");
+            if (node) {
+                document.body.removeChild(node);
+
+                document.body.style.overflowY = "";
+                document.body.style.paddingRight = "";
+
+                const rootElement = document.getElementById("root");
+                rootElement.style.filter = "";
+            }
+        };
+    }, []);
+
+    return portalHost
+        ? ReactDOM.createPortal(props.children, portalHost)
+        : null;
+}
+
 function HUDOverlay({ onCloseOverlayClick, children, modified }) {
     return (
-        <div
-            style={{
-                position: "fixed",
-                width: "100%",
-                minHeight: 100,
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                backgroundColor: "rgba(255,255,255,.9)",
-                zIndex: "20000",
-                display: "flex",
-                flexFlow: "column nowrap",
-            }}
-        >
-            {!modified && (
-                <ButtonBase
-                    style={{
-                        position: "absolute",
-                        bottom: "0%",
-                        right: "0%",
-                        marginRight: "2rem",
-                        marginBottom: "2rem",
-                        backgroundColor: "red",
-                        color: "white",
-                        width: "3rem",
-                        height: "3rem",
-                        borderRadius: "1.5rem",
-                        boxShadow: "3px 3px 3px 0px black",
-                        boxSizing: "border-box",
-                        border: "2px solid white",
-                    }}
-                    onClick={onCloseOverlayClick}
-                >
-                    <AddIcon style={{ transform: "rotate(45deg)" }} />
-                </ButtonBase>
-            )}
-            {modified && (
-                <ButtonBase
-                    style={{
-                        position: "absolute",
-                        bottom: "0%",
-                        right: "0%",
-                        marginRight: "2rem",
-                        marginBottom: "2rem",
-                        backgroundColor: "teal",
-                        color: "white",
-                        width: "3rem",
-                        height: "3rem",
-                        borderRadius: "1.5rem",
-                        boxShadow: "3px 3px 3px 0px black",
-                        boxSizing: "border-box",
-                        border: "2px solid white",
-                    }}
-                    onClick={onCloseOverlayClick}
-                >
-                    <SaveIcon />
-                </ButtonBase>
-            )}
+        <Overlay>
             <div
                 style={{
-                    boxSizing: "border-box",
-                    border: "3px solid gray",
-                    padding: ".2rem",
-                    flex: "1 0 auto",
-                    overflow: "scroll",
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    zIndex: 9999,
+                    display: "grid",
+                    gridTemplateColumns: "1fr",
+                    gridTemplateRows: "1fr auto",
                 }}
             >
-                {children}
+                <div
+                    style={{
+                        gridColumn: "1/2",
+                        gridRow: "1/3",
+                        background: "rgba(0, 0, 0, .4)",
+                    }}
+                />
+                <div
+                    style={{
+                        gridColumn: "1/2",
+                        gridRow: "1/2",
+                        overflow: "scroll",
+                    }}
+                >
+                    {children}
+                </div>
+                <ButtonBase
+                    style={{
+                        backgroundColor: modified ? "teal" : "red",
+                        gridRow: "2/3",
+                        gridColumn: "1/2",
+                        color: "white",
+                        width: "3rem",
+                        height: "3rem",
+                        borderRadius: "1.5rem",
+                        boxShadow: "3px 3px 3px 0px black",
+                        boxSizing: "border-box",
+                        border: "2px solid white",
+                        margin: "1rem 1rem 1rem auto",
+                    }}
+                    onClick={onCloseOverlayClick}
+                >
+                    {modified ? (
+                        <SaveIcon />
+                    ) : (
+                        <AddIcon style={{ transform: "rotate(45deg)" }} />
+                    )}
+                </ButtonBase>
             </div>
-        </div>
+        </Overlay>
     );
 }
 
