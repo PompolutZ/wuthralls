93e854e1aeccd4d674875e42939141deee9aef1d
Oleh Lutsenko <pompolutz@gmail.com>
2021-05-13 08:31:46 +0200

    refactor magic die

--- src/components/MagicDie.js
+++ src/components/MagicDie.js
@@ -1,81 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { MagicChannel, MagicFocus, Crit } from "./CommonSVGs";
+import { makeStyles } from "@material-ui/core/styles";
 
-function MagicDie({ side, size }) {
+const useStyles = makeStyles({
+    core: ({ size }) => {
+        return {
+            width: size,
+            height: size,
+            display: "flex",
+            boxSizing: "border-box",
+            border: `2px solid indigo`,
+            borderRadius: size * 0.2,
+            background:
+                "linear-gradient(212deg, rgba(255,255,255,1) 0%, rgba(195,251,77,1) 20%, rgba(18,255,225,1) 80%, rgba(255,255,255,1) 100%)",
+        };
+    },
+
+    symbol: ({ size }) => ({
+        margin: "auto",
+        width: size * 0.8,
+        height: size * 0.8,
+        color: "indigo",
+    }),
+});
+
+function SideToSymbol({ side, className }) {
+    const value = Number(side);
+    switch (value) {
+        case 1:
+            return <MagicChannel className={className} />;
+        case 2:
+            return <MagicFocus className={className} />;
+        case 3:
+            return <MagicChannel className={className} />;
+        case 4:
+            return <MagicFocus className={className} />;
+        case 5:
+            return <MagicChannel className={className} />;
+        default:
+            return <Crit className={className} />;
+    }
+}
+
+SideToSymbol.propTypes = {
+    side: PropTypes.number,
+    className: PropTypes.string,
+};
+
+function MagicDie(props) {
+    const classes = useStyles(props);
     return (
-        <div
-            style={{
-                width: size,
-                height: size,
-                display: "flex",
-                boxSizing: "border-box",
-                border: `2px solid indigo`,
-                borderRadius: size * 0.2,
-                background:
-                    "linear-gradient(212deg, rgba(255,255,255,1) 0%, rgba(195,251,77,1) 20%, rgba(18,255,225,1) 80%, rgba(255,255,255,1) 100%)",
-            }}
-        >
-            {side === 1 && (
-                <MagicChannel
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
-            {side === 2 && (
-                <MagicFocus
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
-            {side === 3 && (
-                <MagicChannel
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
-            {side === 4 && (
-                <MagicFocus
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
-            {side === 5 && (
-                <MagicChannel
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
-            {side === 6 && (
-                <Crit
-                    style={{
-                        margin: "auto",
-                        width: size * 0.8,
-                        height: size * 0.8,
-                        color: "indigo",
-                    }}
-                />
-            )}
+        <div className={classes.core}>
+            <SideToSymbol className={classes.symbol} {...props} />
         </div>
     );
 }
